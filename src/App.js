46dbdfe07045115406d0695bb8787368e1cb5221
Mohammed Siddique <khanshahid9066@gmail.com>
2025-07-30 19:18:47 +0530

    chat-bot
    framing a basic structure for a chat-bot using react

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import msgIcon from './assets/message.svg';
 import home from './assets/home.svg';
 import saved from './assets/bookmark.svg';
 import rocket from './assets/rocket.svg';
+import sendBtn from './assets/send.svg';
 
 function App() {
   return (
@@ -16,21 +17,26 @@ function App() {
           <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo" /><span className="brand">Chat Bot</span></div>
           <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
           <div className="upperSideBottom">
-            <button className="query"><img src={msgIcon} alt="Query" />What is Programming?</button>
-            <button className="query"><img src={msgIcon} alt="Query" />How to use and API?</button>
+            <button className="query"><img src={msgIcon} alt="Query" />What is Programming ?</button>
+            <button className="query"><img src={msgIcon} alt="Query" />How to use and API ?</button>
           </div>
         </div>
         <div className="lowerSide">
-          <div className="listItems"><img src={home} alt="" className="listItems" />Home</div>
-          <div className="listItems"><img src={saved} alt="" className="listItems" />Saved</div>
-          <div className="listItems"><img src={rocket} alt="" className="listItems" />Upgrade to Pro</div>
+          <div className="listItems"><img src={home} alt="" className="listItemsImg" />Home</div>
+          <div className="listItems"><img src={saved} alt="" className="listItemsImg" />Saved</div>
+          <div className="listItems"><img src={rocket} alt="" className="listItemsImg" />Upgrade to Pro</div>
         </div>
       </div>
 
       {/* main section  */}
 
       <div className="main">
-
+        <div className="chats"></div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" /><button className="send"><img src={sendBtn} alt="" /></button>
+          </div>
+        </div>
       </div>
 
     </div>
